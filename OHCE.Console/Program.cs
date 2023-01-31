bd4842047d76202c461e62a86d5e67732a7268a1
ines-boucard <72189183+ines-boucard@users.noreply.github.com>
2023-01-31 19:32:37 +0100

    projet fonctionnel avec derniers changements

--- OHCE.Console/Program.cs
+++ OHCE.Console/Program.cs
@@ -3,7 +3,7 @@ using OHCE.Console.Adapters;
 
 
 var ohce = new Ohce(SystemLangueAdapter.langueSystem, SystemTimePériodeJournéeAdapter.PériodeActuelle);
-Console.WriteLine(ohce.Saluer());
+//Console.WriteLine(ohce.Saluer());
 Console.WriteLine(">");
 Console.WriteLine(ohce.Palindrome(Console.ReadLine() ?? String.Empty));
-Console.WriteLine(ohce.AuRevoir());
+//Console.WriteLine(ohce.AuRevoir());
